4c75d95f1162287d95f0b3e2dba2ab777ab5c141
TeXider <texider.code@gmail.com>
2025-11-10 14:20:16 +0100

    Fixed get_line() segfault when first line is empty (previous fix was incorrect)

--- dr_mizumis_lab/get_line/history.c
+++ dr_mizumis_lab/get_line/history.c
@@ -6,7 +6,7 @@
 /*   By: almighty <almighty@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 17:13:27 by tpanou-d          #+#    #+#             */
-/*   Updated: 2025/11/10 14:17:04 by almighty         ###   ########.fr       */
+/*   Updated: 2025/11/10 14:19:42 by almighty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,11 @@ inline bool	new_history_entry(t_env *env)
 inline void	remove_new_history_entry(t_env *env)
 {
 	safe_free_line(&env->history->edit_line);
-	env->history = env->history->prev;
-	if (env->history)
+	if (env->history->prev)
+	{
+		env->history = env->history->prev;
 		safe_free((void **) &env->history->next);
+	}
+	else
+		safe_free((void **)&env->history);
 }
