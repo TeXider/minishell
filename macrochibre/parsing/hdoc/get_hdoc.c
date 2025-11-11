099f6936ec3b59a64a47122d6795a287c4fc2ab1
Thibault Panou De Faymoreau <tpanou-d@1G2.42angouleme.fr>
2025-11-11 10:31:50 +0100

    commented main of get_hdoc.c

--- macrochibre/parsing/hdoc/get_hdoc.c
+++ macrochibre/parsing/hdoc/get_hdoc.c
@@ -6,7 +6,7 @@
 /*   By: tpanou-d <tpanou-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 08:55:49 by almighty          #+#    #+#             */
-/*   Updated: 2025/11/11 10:27:43 by tpanou-d         ###   ########.fr       */
+/*   Updated: 2025/11/11 10:30:16 by tpanou-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,46 +74,46 @@ bool	get_hdoc(t_cmd_parsing *cmdp, bool has_expand, t_env *env)
 	return (false);
 }
 
-int	main(int argc, char **argv)
-{
-	(void) argc; (void) argv;
-	t_env	env;
-	env.is_ctrl = false;
-	env.history = NULL;
-	env.empty_list[0] = NULL;
-	env.empty_list[1] = NULL;
-	env.empty_string[0] = '\0';
-	env.envp = malloc(sizeof(char *) * 4);
-	env.envp[3] = NULL;
-	env.envp[0] = "bousiller=   c "; env.envp[1] = "kirikou=balletrou"; env.envp[2] = "fort=rin tin tin ";
-	t_cmd_parsing cmdp;
-	t_cmd		cmd;
-	t_redir		redir;
-	cmdp.argv_len = 0;
-	cmdp.saved_str = NULL;
-	cmdp.sep = ' ';
-	cmdp.cmd = &cmd;
-	cmdp.str = malloc(sizeof(char) * 257);
-	ssize_t rd = read(1, cmdp.str, 256);
-	if (rd == -1)
-		return (1);
-	cmdp.str[rd] = '\0';
-	redir.name = cmdp.str;
-	redir.type = HDOC;
-	cmdp.curr_redir = &redir;
-	redir.name = cmdp.str;
-	cmd.fd_in = -1;
-	cmd.fd_in_type = EMPTY_REDIR;
-	if (get_hdoc(&cmdp, 1, &env))
-		printf("Yo mama so fat she broke my hdocs\n");
-	size_t	i;
-	ssize_t	size;
-	while (true)
-	{
-		size = read(cmd.fd_in, cmdp.str, 1);
-		if (!size)
-			break ;
-		for  (i = 0; i < (size_t)size; i++)
-			write(1, cmdp.str + i, 1);
-	}
-}
+//int	main(int argc, char **argv)
+//{
+//	(void) argc; (void) argv;
+//	t_env	env;
+//	env.is_ctrl = false;
+//	env.history = NULL;
+//	env.empty_list[0] = NULL;
+//	env.empty_list[1] = NULL;
+//	env.empty_string[0] = '\0';
+//	env.envp = malloc(sizeof(char *) * 4);
+//	env.envp[3] = NULL;
+//	env.envp[0] = "bousiller=   c "; env.envp[1] = "kirikou=balletrou"; env.envp[2] = "fort=rin tin tin ";
+//	t_cmd_parsing cmdp;
+//	t_cmd		cmd;
+//	t_redir		redir;
+//	cmdp.argv_len = 0;
+//	cmdp.saved_str = NULL;
+//	cmdp.sep = ' ';
+//	cmdp.cmd = &cmd;
+//	cmdp.str = malloc(sizeof(char) * 257);
+//	ssize_t rd = read(1, cmdp.str, 256);
+//	if (rd == -1)
+//		return (1);
+//	cmdp.str[rd] = '\0';
+//	redir.name = cmdp.str;
+//	redir.type = HDOC;
+//	cmdp.curr_redir = &redir;
+//	redir.name = cmdp.str;
+//	cmd.fd_in = -1;
+//	cmd.fd_in_type = EMPTY_REDIR;
+//	if (get_hdoc(&cmdp, 1, &env))
+//		printf("Yo mama so fat she broke my hdocs\n");
+//	size_t	i;
+//	ssize_t	size;
+//	while (true)
+//	{
+//		size = read(cmd.fd_in, cmdp.str, 1);
+//		if (!size)
+//			break ;
+//		for  (i = 0; i < (size_t)size; i++)
+//			write(1, cmdp.str + i, 1);
+//	}
+//}
