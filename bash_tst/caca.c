11e2f6541a17ba49465ba77d11459892943ab8b1
Thibault Panou De Faymoreau <tpanou-d@1H2.42angouleme.fr>
2025-11-26 12:28:06 +0100

    Added to exec TODO

--- bash_tst/caca.c
+++ bash_tst/caca.c
@@ -2,8 +2,21 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/wait.h>
+#include <string.h>
 
 int main(int argc, char **argv, char **envp)
 {
-	while(1);
+	int pid = fork();
+	if (!pid)
+	{
+		execve("segfault", argv, envp);
+	}
+	int ec;
+	waitpid(pid, &ec, 0);
+	if (WIFEXITED(ec))
+		ec = WEXITSTATUS(ec);
+	else if (WIFSIGNALED(ec))
+		ec = 128 + WTERMSIG(ec);
+	printf("%d: %s\n", ec, strerror(ec));
 }
