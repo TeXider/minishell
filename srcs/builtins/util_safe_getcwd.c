60c44ae18c2e3312f98297207da211c88eff7b09
Thibault Panou De Faymoreau <tpanou-d@1G2.42angouleme.fr>
2025-12-08 09:59:51 +0100

    fixed safe_getcwd() error message

--- srcs/builtins/util_safe_getcwd.c
+++ srcs/builtins/util_safe_getcwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   util_safe_getcwd.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: almighty <almighty@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tpanou-d <tpanou-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 13:58:27 by almighty          #+#    #+#             */
-/*   Updated: 2025/12/01 13:25:28 by almighty         ###   ########.fr       */
+/*   Updated: 2025/12/08 09:56:45 by tpanou-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ bool	safe_getcwd(char **dst, t_env *env)
 	*dst = getcwd(NULL, 0);
 	if (!*dst)
 	{
-		create_error("getcwd() :", SYS_ERR, env);
+		create_error("getcwd()", SYS_ERR, env);
 		return (true);
 	}
 	return (false);
